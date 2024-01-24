db26e56369a5e69e194d08e49a0b298b818a1fa2
MSI\timre <timregan10@gmail.com>
2024-01-24 15:56:39 -0800

    generate diff freq in each ear

--- android/src/main/java/io/github/mertguner/sound_generator/generators/signalDataGenerator.java
+++ android/src/main/java/io/github/mertguner/sound_generator/generators/signalDataGenerator.java
@@ -2,32 +2,27 @@ package io.github.mertguner.sound_generator.generators;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import io.github.mertguner.sound_generator.handlers.getOneCycleDataHandler;
+ 
 
 public class signalDataGenerator {
-
-
     private final float _2Pi = 2.0f * (float) Math.PI;
 
     private int sampleRate = 48000;
     private float phCoefficient = _2Pi / (float) sampleRate;
     private float smoothStep = 1f / (float) sampleRate * 20f;
 
-    private float frequency = 50;
+    private float frequencyLeft = 50;
+    private float oldFrequencyLeft = 50;
+    private float phLeft = 0;
+    private float frequencyRight = 50;
+    private float oldFrequencyRight = 50;
+    private float phRight = 0;
     private baseGenerator generator = new sinusoidalGenerator();
 
     private short[] backgroundBuffer;
     private short[] buffer;
-    private List<Integer> oneCycleBuffer = new ArrayList<>();
     private int bufferSamplesSize;
-    private float ph = 0;
-    private float oldFrequency = 50;
     private boolean creatingNewData = false;
-    private boolean autoUpdateOneCycleSample = false;
-
-    public boolean isAutoUpdateOneCycleSample() { return autoUpdateOneCycleSample; }
-    public void setAutoUpdateOneCycleSample(boolean autoUpdateOneCycleSample) { this.autoUpdateOneCycleSample = autoUpdateOneCycleSample; }
 
     public int getSampleRate() { return sampleRate; }
     public void setSampleRate(int sampleRate) {
@@ -41,15 +36,19 @@ public class signalDataGenerator {
     }
     public void setGenerator(baseGenerator generator) {
         this.generator = generator;
-        createOneCycleData();
     }
 
-    public float getFrequency() {
-        return frequency;
+    public float getLeftEarFrequency() {
+        return frequencyLeft;
     }
-    public void setFrequency(float frequency) {
-        this.frequency = frequency;
-        createOneCycleData();
+    public float getRightEarFrequency() {
+        return frequencyRight;
+    }
+    public void setLeftEarFrequency(float frequency) {
+        this.frequencyLeft = frequency;
+    }
+    public void setRightEarFrequency(float frequency) {
+        this.frequencyRight = frequency;
     }
 
     public signalDataGenerator(int bufferSamplesSize, int sampleRate) {
@@ -58,21 +57,33 @@ public class signalDataGenerator {
         buffer = new short[bufferSamplesSize];
         setSampleRate(sampleRate);
         updateData();
-        createOneCycleData();
     }
 
     private void updateData() {
         creatingNewData = true;
         for (int i = 0; i < bufferSamplesSize; i++) {
-            oldFrequency += ((frequency - oldFrequency) * smoothStep);
-            backgroundBuffer[i] = generator.getValue(ph, _2Pi);
-            ph += (oldFrequency * phCoefficient);
+            if (i % 2 == 0) { // even numbers are left ear and odd numbers are right ear
+                oldFrequencyLeft += ((frequencyLeft - oldFrequencyLeft) * smoothStep);
+                backgroundBuffer[i] = generator.getValue(phLeft, _2Pi);
+                phLeft += (oldFrequencyLeft * phCoefficient);
 
-            //performance of this block is higher than ph %= _2Pi;
-            // ifBlock  Test score =  2,470ns
-            // ModBlock Test score = 27,025ns
-            if (ph > _2Pi) {
-                ph -= _2Pi;
+                //performance of this block is higher than ph %= _2Pi;
+                // ifBlock  Test score =  2,470ns
+                // ModBlock Test score = 27,025ns
+                if (phLeft > _2Pi) {
+                    phLeft -= _2Pi;
+                }
+            } else {
+                oldFrequencyRight += ((frequencyRight - oldFrequencyRight) * smoothStep);
+                backgroundBuffer[i] = generator.getValue(phRight, _2Pi);
+                phRight += (oldFrequencyRight * phCoefficient);
+
+                //performance of this block is higher than ph %= _2Pi;
+                // ifBlock  Test score =  2,470ns
+                // ModBlock Test score = 27,025ns
+                if (phRight > _2Pi) {
+                    phRight -= _2Pi;
+                }
             }
         }
         creatingNewData = false;
@@ -89,23 +100,5 @@ public class signalDataGenerator {
             }).start();
         }
         return this.buffer;
-    }
-
-    public void createOneCycleData() {
-        createOneCycleData(false);
-    }
-
-    public void createOneCycleData(boolean force) {
-        if (generator == null || (!autoUpdateOneCycleSample && !force))
-            return;
-
-        int size = Math.round(_2Pi / (frequency * phCoefficient));
-
-        oneCycleBuffer.clear();
-        for (int i = 0; i < size; i++) {
-            oneCycleBuffer.add((int)generator.getValue((frequency * phCoefficient) * (float) i, _2Pi));
-        }
-        oneCycleBuffer.add((int)generator.getValue(0, _2Pi));
-        getOneCycleDataHandler.setData(oneCycleBuffer);
     }
 }
